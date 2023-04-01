c88f9a1a8995d4ea9410c4163c2ce2e526841104
ilonavarro <ilo.navarro+github@gmail.com>
2023-04-01 20:17:20 +0100

    header and footer ok

---
+++ components/Footer/index.js
@@ -0,0 +1,14 @@
+import styles from './Footer.module.css'
+
+// let year = new Date().getFullYear()
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        {/* <span>PokeNext</span> &copy; {year} */}
+        <span>PokeNext</span> &copy; 2023
+      </p>
+    </footer>
+  )
+}
