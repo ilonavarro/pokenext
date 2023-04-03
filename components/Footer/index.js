641ff6936a5f598b6c18a249c6bd0f7da0bf62be
ilonavarro <ilo.navarro+github@gmail.com>
2023-04-03 16:43:40 +0100

    Dynamic date in the footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,13 +1,13 @@
 import styles from './Footer.module.css'
 
-// let year = new Date().getFullYear()
+let year = new Date().getFullYear()
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <p>
-        {/* <span>PokeNext</span> &copy; {year} */}
-        <span>PokeNext</span> &copy; 2023
+        <span>PokeNext</span> &copy; {year}
+        {/* <span>PokeNext</span> &copy; 2023 */}
       </p>
     </footer>
   )
